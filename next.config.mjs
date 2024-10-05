ff1c3ec7027fcea5e415d06c389174c4ffc3fca6
Yudi Alviska <yudi.a@weefer.co.id>
2024-10-05 21:10:58 +0700

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost', 'api.twillink.com'],
+  },
+  output: 'standalone',
+};
 
 export default nextConfig;
