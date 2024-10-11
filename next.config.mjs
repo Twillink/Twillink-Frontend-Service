1d55d58f4700667e04dfe3a20638b6c0d6ee5de8
Yudi Alviska <yudi.a@weefer.co.id>
2024-10-11 14:21:58 +0700

    test fix env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'api.twillink.com', 'api.dev.twillink.com'],
   },
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   output: 'standalone',
 };
 
