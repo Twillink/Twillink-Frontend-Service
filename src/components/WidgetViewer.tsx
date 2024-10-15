33919cbee735cb2c607194c0dea58aa7abaa3bf9
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2024-10-15 15:21:02 +0700

    fix: props in widget viewer

--- src/components/WidgetViewer.tsx
+++ src/components/WidgetViewer.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, {useRef} from 'react';
+import React, {useMemo, useRef} from 'react';
 import ScrollHideHeader from '@/components/widgets/ScrollHideHeader';
 import UserProfile from '@/components/widgets/UserProfile';
 import SocialContainer from '@/components/widgets/SocialContainer';
 import WidgetContainerViewer from './widgets/WidgetContainerViewer';
 import {IItemWidgetType} from '@/libs/types/IItemWidgetType';
+import {WidgetTypeEnum} from '@/libs/types/WidgetTypeEnum';
 
 interface IWidgetViewer {
   dataWidget: IItemWidgetType[];
@@ -13,6 +14,21 @@ interface IWidgetViewer {
 
 const WidgetViewer: React.FC<IWidgetViewer> = ({dataWidget}) => {
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const dataContact = useMemo(() => {
+    return dataWidget.filter(item => item.type === WidgetTypeEnum.Contact);
+  }, [dataWidget]);
+
+  const dataWidgetFiltered = useMemo(() => {
+    return dataWidget.filter(
+      item =>
+        ![WidgetTypeEnum.Contact, WidgetTypeEnum.Social].includes(item.type),
+    );
+  }, [dataWidget]);
+
+  const dataSocial = useMemo(() => {
+    return dataWidget.filter(item => item.type === WidgetTypeEnum.Social);
+  }, [dataWidget]);
   return (
     <>
       <div data-theme="light" className="h-full max-w-[428px]">
@@ -20,10 +36,10 @@ const WidgetViewer: React.FC<IWidgetViewer> = ({dataWidget}) => {
           className="artboard flex flex-col bg-base-100 h-full overflow-y-auto relative"
           ref={scrollContainerRef}>
           <ScrollHideHeader />
-          <UserProfile />
+          <UserProfile contact={dataContact} />
           <div className="flex flex-wrap px-6">
-            <SocialContainer />
-            {dataWidget
+            <SocialContainer data={dataSocial} />
+            {dataWidgetFiltered
               .sort((a, b) => a.order - b.order)
               .map((widget, index) => (
                 <WidgetContainerViewer
