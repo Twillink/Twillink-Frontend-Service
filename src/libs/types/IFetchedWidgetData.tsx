357a6e9a4b1ac1774d8c1d60834f980202e412bf
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2024-10-18 17:12:28 +0700

    refactor: separate object interfaces

--- src/libs/types/IFetchedWidgetData.tsx
+++ src/libs/types/IFetchedWidgetData.tsx
@@ -1,15 +1,39 @@
 import {WidgetTypeEnum} from './WidgetTypeEnum';
 
+export interface IFetchedWidgetLink {
+  title: string;
+  url: string;
+  image?: string;
+}
+
+export interface IFetchedWidgetText {
+  text: string;
+}
+
+export interface IFetchedWidgetImage {
+  caption: string;
+  url: string;
+  attachmentId?: string;
+}
+
+export interface IFetchedWidgetVideo {
+  caption: string;
+  url: string;
+  attachmentId?: string;
+}
+
+export interface IFetchedWidgetContact {
+  email: string;
+  phoneNumber: string;
+}
+
 export interface IFetchedWidgetData {
   id: number;
   sequence: number;
   typeWidget: WidgetTypeEnum;
-  widgetLink?: {
-    title: string;
-    url: string;
-    image: string;
-  } | null;
-  widgetText?: {
-    text: string;
-  } | null;
+  widgetLink?: IFetchedWidgetLink | null;
+  widgetText?: IFetchedWidgetText | null;
+  widgetImage?: IFetchedWidgetImage | null;
+  widgetVideo?: IFetchedWidgetVideo | null;
+  widgetContact?: IFetchedWidgetContact | null;
 }
