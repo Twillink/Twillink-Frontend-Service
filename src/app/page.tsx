56fae9a4db377770cf91db3a5e05324f78b15d8e
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2024-11-05 15:26:43 +0700

    feat: intro page and animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import SvgTwillinkLogo from '@/assets/svgComponents/SvgTwillinkLogo';
 import Link from 'next/link';
 import {useAppSelector} from '@/libs/hooks/useReduxHook';
 import {RootState} from '@/libs/store/store';
+import {RainbowButton} from '@/components/Button/RainbowButton';
+import {MotionDiv, MotionImg} from '@/libs/motion/motion';
 
 const Home: React.FC = () => {
   const isLoggedIn = useAppSelector(
@@ -12,35 +14,123 @@ const Home: React.FC = () => {
   );
   return (
     <div
-      data-theme="skinLight"
-      className="h-screen w-screen flex flex-col items-center justify-center px-2 gap-3">
-      <SvgTwillinkLogo height={30} className="fill-logo" />
-      <h1 className="text-center text-3xl font-bold text-primary leading-relaxed">
-        Your exceptional Link in Bio
-      </h1>
-      <h2 className="text-primary font-semibold leading-relaxed">
-        Claim your username now
-      </h2>
-      {isLoggedIn ? (
-        <div>
-          <Link href="/admin">
-            <Button title="Go to Admin" />
-          </Link>
+      data-theme={'skinLight'}
+      className={'p-16 flex h-screen justify-center items-center gap-[20%]'}>
+      <MotionDiv
+        initial={{opacity: 0, x: -100}}
+        whileInView={{opacity: 1, x: 0}}
+        transition={{
+          ease: 'easeInOut',
+          duration: 1,
+          delay: 1,
+        }}
+        data-theme="skinLight"
+        className="flex flex-col items-start justify-center px-2 gap-4">
+        <SvgTwillinkLogo height={30} className="fill-logo" />
+        <h1 className="text-start text-4xl font-bold text-primary leading-relaxed">
+          Your exceptional <span className={'block'}>Link in Bio</span>
+        </h1>
+        <h2 className="text-primary text-[20px] font-normal leading-relaxed mb-4">
+          Claim your username now
+        </h2>
+        {isLoggedIn ? (
+          <div>
+            <Link href="/admin">
+              <Button title="Go to Admin" />
+            </Link>
+          </div>
+        ) : (
+          <div className={'flex flex-row  items-center gap-4'}>
+            <div>
+              <Link href="/signup">
+                <RainbowButton>Create your Twillink</RainbowButton>
+              </Link>
+            </div>
+            <div>
+              <p className={''}>or</p>
+            </div>
+            <div>
+              <Link href="/login" className="hover:underline">
+                <Button title={'Login'} color={'outline'} />
+              </Link>
+            </div>
+          </div>
+        )}
+      </MotionDiv>
+      <div>
+        <div className={'relative'}>
+          <div>
+            <MotionImg
+              initial={{opacity: 0, y: 100}}
+              whileInView={{opacity: 1, y: 0}}
+              transition={{
+                ease: 'easeInOut',
+                duration: 1,
+                delay: 1,
+              }}
+              src="/images/Mobile Preview.png"
+              alt="Preview"
+              className={'w-80'}
+            />
+          </div>
+          <MotionDiv
+            initial={{opacity: 0, x: -100}}
+            whileInView={{opacity: 1, x: 0}}
+            transition={{
+              ease: 'easeInOut',
+              duration: 1,
+              delay: 2,
+            }}
+            className={'absolute top-[14%] left-[-50%]'}>
+            <MotionImg
+              initial={{
+                transform: 'translateZ(8px) translateY(-2px)',
+              }}
+              animate={{
+                transform: 'translateZ(32px) translateY(-8px)',
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: 'mirror',
+                duration: 2,
+                ease: 'easeInOut',
+                delay: 3,
+              }}
+              src="/images/calendar.png"
+              alt="Preview"
+              className={'w-60 '}
+            />
+          </MotionDiv>
+          <MotionDiv
+            initial={{opacity: 0, y: 10, scale: 0.5}}
+            whileInView={{opacity: 1, y: 0, scale: 1}}
+            transition={{
+              ease: 'easeInOut',
+              duration: 1,
+              delay: 2,
+            }}
+            className={' absolute bottom-[8%] right-[-30%]'}>
+            <MotionImg
+              initial={{
+                transform: 'translateZ(8px) translateY(-2px)',
+              }}
+              animate={{
+                transform: 'translateZ(32px) translateY(-8px)',
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: 'mirror',
+                duration: 2,
+                ease: 'easeInOut',
+                delay: 3,
+              }}
+              src="/images/blog.png"
+              alt="Preview"
+              className="h-[168px]"
+            />
+          </MotionDiv>
         </div>
-      ) : (
-        <>
-          <div>
-            <Link href="/signup">
-              <Button title="Create your Twillink" />
-            </Link>
-          </div>
-          <div>
-            <Link href="/login" className="hover:underline">
-              Log In
-            </Link>
-          </div>
-        </>
-      )}
+      </div>
     </div>
   );
 };
