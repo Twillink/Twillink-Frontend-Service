f8f71ba8d18afd02539dae9479d092254df8a6a3
hilmanzu <hilmanzutech@gmail.com>
2024-12-28 14:15:42 +0800

    feat:

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import { useSearchParams } from 'next/navigation';
-import PreJoin from './prejoin';
+import dynamic from 'next/dynamic';
+
+const PreJoin = dynamic(() => import('./prejoin'), { ssr: false });
 
 const Room = () => {
   const searchParams = useSearchParams();
   const meetingId = searchParams.get('id'); // Get the 'id' parameter from the URL
-  console.log(meetingId)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
